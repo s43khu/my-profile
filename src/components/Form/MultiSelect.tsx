ad936b9c957791cf0236a334b16c4d5915df8f55
shekhu <s43khu@gmail.com>
2024-03-16 17:29:03 +0530

    multislect is used wrong

--- src/components/Form/MultiSelect.tsx
+++ src/components/Form/MultiSelect.tsx
@@ -1,53 +1,54 @@
-// import Select from "react-select";
-// import makeAnimated from "react-select/animated";
-// import { FieldWrapper, FieldWrapperPassThroughProps } from "./FieldWrapper";
-// import { Controller, UseFormRegisterReturn, Control } from "react-hook-form";
+import React from "react";
+import Select from "react-select";
+import makeAnimated from "react-select/animated";
+import { FieldWrapper, FieldWrapperPassThroughProps } from "./FieldWrapper";
+import { Controller, UseFormRegisterReturn, Control } from "react-hook-form";
 
-// const animatedComponents = makeAnimated();
+const animatedComponents = makeAnimated();
 
-// type Option = {
-//   label: React.ReactNode;
-//   value: string | number;
-// };
+type Option = {
+  label: React.ReactNode;
+  value: string | number;
+};
 
-// type InputFieldProps = FieldWrapperPassThroughProps & {
-//   options: Option[];
-//   registration: Partial<UseFormRegisterReturn>;
-//   control: Control<any>;
-// };
+type InputFieldProps = FieldWrapperPassThroughProps & {
+  options: Option[];
+  registration?: Partial<UseFormRegisterReturn>;
+  control: Control<any>;
+};
 
-// export const MultiSelect = (props: InputFieldProps) => {
-//   const { registration, error, options, label, control } = props;
-//   const { name } = registration;
+export const MultiSelect = (props: InputFieldProps) => {
+  const { registration = {}, error, options, label, control } = props;
+  const { name } = registration;
 
-//   return (
-//     <FieldWrapper error={error} label={label}>
-//       <Controller
-//         name={name ?? ""}
-//         control={control}
-//         render={({ field: { value, onChange } }) => (
-//           <Select<Option, true>
-//             styles={{
-//               control: (baseStyles) => ({
-//                 ...baseStyles,
-//                 lineHeight: 2,
-//                 borderColor: error?.message ? "red" : "#dee2e6",
-//                 borderRadius: "0.375rem",
-//               }),
-//             }}
-//             closeMenuOnSelect={false}
-//             components={animatedComponents}
-//             onChange={(newVal) => {
-//               const values = newVal.map((i) => i.value);
-//               onChange(values);
-//             }}
-//             defaultValue={options.filter((i) => value?.includes(i.value))}
-//             isMulti
-//             placeholder={label}
-//             options={options}
-//           />
-//         )}
-//       />
-//     </FieldWrapper>
-//   );
-// };
+  return (
+    <FieldWrapper error={error} label={label}>
+      <Controller
+        name={name ?? ""}
+        control={control}
+        render={({ field: { value, onChange } }) => (
+          <Select<Option, true>
+            styles={{
+              control: (baseStyles) => ({
+                ...baseStyles,
+                lineHeight: 2,
+                borderColor: error?.message ? "red" : "#dee2e6",
+                borderRadius: "0.375rem",
+              }),
+            }}
+            closeMenuOnSelect={false}
+            components={animatedComponents}
+            onChange={(newVal) => {
+              const values = newVal.map((i) => i.value);
+              onChange(values);
+            }}
+            defaultValue={options.filter((i) => value?.includes(i.value))}
+            isMulti
+            placeholder={label}
+            options={options}
+          />
+        )}
+      />
+    </FieldWrapper>
+  );
+};
